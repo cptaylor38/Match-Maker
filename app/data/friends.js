bc771671f4171dd8979bc7cb2453e716045e874c
Corbin Taylor <cptaylor38@yahoo.com>
2019-04-18 04:26:46 -0400

    added directories

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+// array of objects
+//ex:
+
+{
+    'name': 'Ahmed',
+        'photo': 'https:imagelinkforexample.jpg',
+            'scores': [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+}
---
+++ routing/apiRoutes.js
@@ -0,0 +1,10 @@
+$.get('/api/friends') //displays json of all possible friends
+
+$.post('/api/friends', function () {
+    for (var i = 0; i < something.length; i++) {
+
+    }
+});
+
+
+//handle incoming survey results and compatability logic
