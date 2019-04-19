ff8bd038c0bbfa87fa6ff9e06996394606473d86
Corbin Taylor <cptaylor38@yahoo.com>
2019-04-19 02:30:56 -0400

    added css to survey page

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,21 @@
 // array of objects
 //ex:
 
-{
-    'name': 'Ahmed',
-        'photo': 'https:imagelinkforexample.jpg',
-            'scores': [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
-}
+// {
+//     'name': 'Ahmed',
+//         'photo': 'https:imagelinkforexample.jpg',
+//             'scores': [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+// }
+
+
+var usersArray = [
+    {
+        userName: "Saima",
+        photoURL: "saima@example.com",
+        userBio: "blah blah blah",
+        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }
+];
+
+module.exports = usersArray;
+
--- server.js
+++ server.js
@@ -1,31 +1,39 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
 var express = require("express");
 
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 var app = express();
 
+// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-var mysql = require("mysql");
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "task_saver_db"
-});
-
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
 });
